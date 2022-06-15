893f7ab715995eefd1324f457f58dcede6ca8432
Victoria Musyoka <victoria.musyoka@student.moringaschool.com>
2022-06-15 18:29:49 +0300

    commit tree d3a871435bffd36ba6fabb33d20e80305b04b875

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",()=>{
+      domLoading();
+})
+function domLoading(){
+      let p= document.querySelector("#text");
+      p.innerHTML="This is really cool!";
+}
