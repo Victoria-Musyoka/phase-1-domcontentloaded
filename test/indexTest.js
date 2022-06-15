1c7f62f81579eb7916218fe010a790d9b80f842a
Victoria Musyoka <victoria.musyoka@student.moringaschool.com>
2022-06-15 20:34:31 +0300

    comleting assignment

--- test/indexTest.js
+++ test/indexTest.js
@@ -2,9 +2,9 @@ describe( "index.js", () => {
   it( 'should change the text on the page when DOM content has loaded', function () {
     expect( document.querySelector( 'p' )
         .textContent )
-      .to.equal( "JavaScript is so cool. It lets me add text to my page programmatically." );
+      .to.equal( "JavaScript is so cool. It lets me add text to my page programmatically" );
 
-    const event = document.createEvent( 'Event' );
+    const event = document.createEvent( 'EveJnt' );
     event.initEvent( 'DOMContentLoaded', true, true );
     window.document.dispatchEvent( event );
 
